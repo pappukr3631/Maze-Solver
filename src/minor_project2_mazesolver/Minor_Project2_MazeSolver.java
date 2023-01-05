40f91a7bf39b833bd25826c37ca84946c9a58aff
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-01-05 18:12:02 +0530

    first commit

---
+++ src/minor_project2_mazesolver/Minor_Project2_MazeSolver.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minor_project2_mazesolver;
+
+/**
+ *
+ * @author pappu
+ */
+public class Minor_Project2_MazeSolver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
